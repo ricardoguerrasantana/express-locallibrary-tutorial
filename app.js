3b05f37d90f2df5765f198347eb419992579e868
Ricardo Guerra <ricardo.andres.guerra.santana@gmail.com>
2021-04-24 12:30:52 +1000

    Merged prod branch

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = process.env.MONGODB_URI || process.env.MONGODB_URI_DEV;
+const mongoDB = process.env.MONGODB_URI_PROD || process.env.MONGODB_URI_DEV;
 mongoose.connect(mongoDB , { useNewUrlParser: true , useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error' , console.error.bind(console , 'MongoDB connection error:'));
